93008c70ef5826c81d11cfaffb6c1bf10ecbeed4
Ravichandra Boggula <bravichandra12@gmail.com>
2024-05-18 12:54:28 +0530

    Update RPS.js

--- RPS.js
+++ RPS.js
@@ -4,13 +4,16 @@ let compscore=0;
 const msg=document.getElementById('msg');
 const message=document.querySelector(".message");
 
-const beat=document.getElementById('beat'); const you=document.getElementById('you'); const bot=document.getElementById('bot');
+const beat=document.getElementById('beat'); 
+const you=document.getElementById('you'); 
+const bot=document.getElementById('bot');
 const setuser=document.querySelector("#user-score");
 const setcomp=document.querySelector("#comp-score");
 
 const generate=() =>{
     const options= ["rock","paper","scissor"];
-    const i= Math.floor(Math.random()*3); return options[i];
+    const i= Math.floor(Math.random()*3); 
+    return options[i];
 }
 
 function gameplay(userchoice){
@@ -39,7 +42,7 @@ function draw(userchoice,compchoice){
     console.log("draw");
     msg.innerText= "It is a Draw";
     message.style.backgroundColor ="rgb(169, 154, 87)";
-     beat.innerText=`${userchoice} draws ${compchoice}`;
+    beat.innerText=`${userchoice} draws ${compchoice}`;
 
 }
 function winner(user,userchoice,compchoice){
@@ -47,18 +50,22 @@ function winner(user,userchoice,compchoice){
         userscore++;
         setuser.innerText= userscore; console.log("win");
         msg.innerText= "You Win!";
-        message.style.backgroundColor ="rgb(29, 189, 136)"; beat.innerText=`${userchoice} beats ${compchoice}`;
+        message.style.backgroundColor ="rgb(29, 189, 136)"; 
+        beat.innerText=`${userchoice} beats ${compchoice}`;
     }
     else{
         compscore++;
         setcomp.innerText= compscore; console.log("lose");
         msg.innerText= "You lose";
-        message.style.backgroundColor ="#6e0d25"; beat.innerText=`${compchoice} beats ${userchoice}`;
+        message.style.backgroundColor ="#6e0d25"; 
+        beat.innerText=`${compchoice} beats ${userchoice}`;
     }
     if(userscore>compscore){
-        bot.style.boxShadow= "10px 7px 20px black inset"; you.style.boxShadow= "10px 7px 20px black";}
+        bot.style.boxShadow= "10px 7px 20px black inset"; 
+        you.style.boxShadow= "10px 7px 20px black";}
     else if(userscore===compscore){
-        bot.style.boxShadow= "10px 7px 20px black"; you.style.boxShadow= "10px 7px 20px black";
+        bot.style.boxShadow= "10px 7px 20px black"; 
+        you.style.boxShadow= "10px 7px 20px black";
     }
     else{
         bot.style.boxShadow= "10px 7px 20px black"; 
@@ -67,7 +74,6 @@ function winner(user,userchoice,compchoice){
 }
 choices.forEach((choice) =>{
     choice.addEventListener("click",function(){
-
         const userchoice=choice.getAttribute("id");
         gameplay(userchoice);
     })
